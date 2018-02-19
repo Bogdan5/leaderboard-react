e919cd651e8501ee5c09b7d926201e1d2d28b54c
Bogdan5 <bogdan5x@gmail.com>
2018-02-18 21:28:17 -0500

    'comment'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import Cards from './components/Cards';
-import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   constructor (props){
     super(props);
-    console.log("construct");
     this.state = {data:[{"username":"sjames1958gm","img":"https://avatars1.githubusercontent.com/u/4639625?v=4","alltime":8597,"recent":116,"lastUpdate":"2018-02-03T18:14:36.182Z"}], isRecent:true};
   }
-
+  // method fetches the data, sorts it and then sets it as state
   getData = () => {
     fetch(this.state.isRecent?'https://fcctop100.herokuapp.com/api/fccusers/top/recent':
       'https://fcctop100.herokuapp.com/api/fccusers/top/alltime')
@@ -18,9 +16,7 @@ class App extends Component {
           if (response.status !== 200){
             console.log("Something wrong: "+response.status);
           }
-          console.log("stuff");
           response.json().then((dat)=>{
-            console.log(dat);
             dat.sort((a,b)=>this.state.isRecent?b.recent-a.recent:b.alltime-a.alltime);
             this.setState({data:dat});
           });
@@ -28,16 +24,18 @@ class App extends Component {
       )
       .catch((error)=>{console.log("Error:"+error)})
   };
+
   componentDidMount(){
     this.getData();
   }
-
+  // reinitializes the data if the recent column head is clicked
   onrecent=()=>{
     if (!this.state.isRecent){
       this.setState({isRecent:true});
       this.getData();
     }
   }
+  // reinitializes the data if the alltime column head is clicked
   onalltime=()=>{
     if (this.state.isRecent){
       this.setState({isRecent:false});
@@ -46,15 +44,20 @@ class App extends Component {
   }
 
   render() {
+    let index=0;  //the index in the number column
     return (
       // addCards(this.state.recentOn)
       <div className="wrapper">
-        <div onClick={this.onrecent}>
-          Recent
+        <div>Leaderboard</div>
+        <div className="columns">
+          <div>No.</div>
+          <div>Name</div>
+          <div onClick={this.onrecent}>Points in the last 30 days{this.state.isRecent?"\u25BC":''}</div>
+          <div onClick={this.onalltime}>Alltime points{this.state.isRecent?'':"\u25BC"}</div>
         </div>
-        <div onClick={this.onalltime}>Alltime</div>
         {
-          this.state.data.map((item)=>(<Cards key={item.username} name={item.username}
+          this.state.data.map((item)=>
+            (<Cards key={item.username} number={++index} name={item.username}
             urlImg={item.img} alltime={item.alltime}
             recent={item.recent}/>))
         }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../App.css';
 
+// this component includes all the information for each entry on the leaderboard
 class Cards extends Component {
-
 
   render() {
     return (
       <div className="card-container">
-        <div>{this.props.username}</div>
-        <div>{this.props.urlImg}</div>
-        <div>{this.props.alltime}</div>
+        <div>{this.props.number}</div>
+        <div><img src={this.props.urlImg} alt=""/><section>{this.props.name}</section></div>
         <div>{this.props.recent}</div>
+        <div>{this.props.alltime}</div>
       </div>
     );
   }
